c14b0c902c4c3e4db97931e846bbae01f6d3fcc4
OvchinnikovMaxim <77479094+OvchinnikovMaxim@users.noreply.github.com>
2021-07-06 10:42:25 +0300

    Add files via upload

---
+++ копирование скриптов.bat
@@ -0,0 +1,14 @@
+
+
+@Echo Копирование файлов из C:\Users\ovchinnikov\Desktop\scripts в K:\Персонал\Овчинников М.И\scripts 
+
+xcopy  "C:\Users\ovchinnikov\Desktop\scripts" "K:\Персонал\Овчинников М.И\scripts" /y
+
+@Echo --------------------------------------------------------------
+rem Echo.
+@Echo Копирование файлов из C:\Users\ovchinnikov\Desktop\text doc в K:\Персонал\Овчинников М.И\text doc
+
+xcopy /y "C:\Users\ovchinnikov\Desktop\text doc" "K:\Персонал\Овчинников М.И\text doc" 
+
+
+
